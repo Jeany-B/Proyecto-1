e066edfe774aea09b4a2ad134576c74862f701c9
Jeany-B <jeany.aravena.l@hotmail.com>
2024-08-30 09:32:14 -0400

    Se modifica el HOST del archivo client.py para que tenga acceso al servidor en el EV3

--- src/ev3/client.py
+++ src/ev3/client.py
@@ -3,11 +3,14 @@ from tkinter import *
 from tkinter import ttk
 
 def retornar_socket():
-    HOST = "localhost"
+    #"192.168.28.123"
+    #"localhost"
+    HOST = "192.168.28.123"
     PORT = 8080
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
+    print("Socket creado")
 
 
     return s
