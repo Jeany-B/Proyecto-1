e995e36e7c4a6163742fd92c837ea6a448e4358a
Jeany-B <jeany.aravena.l@hotmail.com>
2024-09-04 15:17:05 -0400

    Actualización menor del archivo

--- src/ev3/prueba_mando.py
+++ src/ev3/prueba_mando.py
@@ -1,48 +1,31 @@
 import pygame
 
+#Joysticks
 pygame.joystick.init()
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 
-class Player(object):
-
-    def __init__(self):
-        self.player = pygame.rect.Rect((300, 400, 50, 50))
-        self.color = "white"
-
-    def move(self, x_speed, y_speed):
-        self.player.move_ip((x_speed, y_speed))
-
-    def change_color(self, color):
-        self.color = color
-
-    def draw(self, game_screen):
-        pygame.draw.rect(game_screen, self.color, self.player)
-
 
 pygame.init()
-player = Player()
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode((800, 600))
-
-while True:
+running = True
+while running:
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
-            break
+            running = False
 
         #Se reconoce el movimiento con la funcion JOYHBUTTONDOWN (botones principales)
         if event.type == pygame.JOYBUTTONDOWN:
             if pygame.joystick.Joystick(0).get_button(0):
-                player.change_color("blue")
+                print("X")
 
             elif pygame.joystick.Joystick(0).get_button(1):
-                player.change_color("red")
+                print("O")
 
             elif pygame.joystick.Joystick(0).get_button(2):
-                player.change_color("pink")
+                print("CUADRADO")
 
             elif pygame.joystick.Joystick(0).get_button(3):
-                player.change_color("green")
+                print("Triangulo")
 
         #Se reconoce el movimiento con la funcion JOYHATMOTION (flechitas)
         if event.type == pygame.JOYHATMOTION:
@@ -61,13 +44,3 @@ while True:
             else:
                 #print("Centro")
                 pass
-    
-    #x_speed = round(pygame.joystick.Joystick(0).get_axis(0))
-    #y_speed = round(pygame.joystick.Joystick(0).get_axis(4))
-    #player.move(x_speed, y_speed)
-
-    screen.fill((0,0,0))
-    player.draw(screen)
-    pygame.display.update()
-
-    clock.tick(180)
