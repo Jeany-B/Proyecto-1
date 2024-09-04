9344fc471352a1d9eb5d38ca9bbb8a502dbe427c
Jeany-B <jeany.aravena.l@hotmail.com>
2024-09-04 15:20:35 -0400

    Se modifica el archivo main.py para la creación del multiprocessing

---
+++ src/ev3/main.py
@@ -0,0 +1,19 @@
+import multiprocessing
+from tkinter_process import tkinter_process
+from pygame_process import pygame_process
+
+if __name__ == "__main__":
+    # Crear la cola de comunicación
+    queue = multiprocessing.Queue()
+
+    # Crear los procesos
+    tkinter_p = multiprocessing.Process(target=tkinter_process, args=(queue,))
+    pygame_p = multiprocessing.Process(target=pygame_process, args=(queue,))
+
+    # Iniciar los procesos
+    tkinter_p.start()
+    pygame_p.start()
+
+    # Esperar a que ambos procesos terminen
+    tkinter_p.join()
+    pygame_p.join()
