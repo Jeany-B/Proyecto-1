4d99b34a4222716c49a818d526f84579725d8e61
Jeany-B <jeany.aravena.l@hotmail.com>
2024-09-06 10:21:23 -0400

    Cambios a server.py

--- src/ev3/server.py
+++ src/ev3/server.py
@@ -1,40 +1,61 @@
-import socket
+#!/usr/bin/env python3
+# encoding=utf-8
 
-HOST = "localhost"
+import socket
+import library as lib
+
+HOST = ""
 PORT = 8080
 
 # Creación y setup de socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Socket creado")
     s.bind((HOST, PORT))
-    print(f"El socket se creó con puerto: {PORT}")
+    print("El socket se creo con puerto: " + str(PORT))
     s.listen(4)
-    print("El socket está escuchando...")
+    print("El socket esta escuchando...")
 
     # Se espera la conexión con el cliente
     client, addr = s.accept()
-    print(f"Se conectó a {addr}")
+    print("Se conecto a " + str(addr))
 
     # Main loop
     while True:
         # Se recibe un byte y se decodifica
-        data = client.recv(128)
-        char = data.decode()
+        data = client.recv(16)
+        key = data.decode()
 
-        match char:
-            case 'w': 
-                print('w')
-            case 'a':
-                print('a')
-            case 's':
-                print('s')
-            case 'd':
-                print('d')
-            case 'q':
-                print("Cerrando conexión...")
-                break
+        if key == 'w':
+            lib.avanzar()
+        elif key == 'a':
+            lib.girar_izquierda()
+        elif key == 's':
+            lib.retroceder()
+        elif key == 'd':
+            lib.girar_derecha()
+        elif key == 'space':
+            lib.hablar()
+        elif key == "ARRIBA":
+            lib.avanzar()
+        elif key == "IZQUIERDA":
+            print(key)
+        elif key == "DERECHA":
+            print(key)
+        elif key == "ABAJO":
+            print(key)
+        elif key == "EQUIS":
+            print(key)
+        elif key == "O":
+            print(key)
+        elif key == "CUADRADO":
+            print(key)
+        elif key == "TRIANGULO":
+            print(key)
+        elif key == 'q':
+            print('q')
+            break
 
-            case _:
-                print("Tecla no soportada")
-
+    print("Cerrando client")
     client.close()
+    print("Cerrando socket")
+    s.close()
