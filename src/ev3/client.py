963affade9230a5ea0f71921b18e42ae6e5b8dab
Jeany-B <jeany.aravena.l@hotmail.com>
2024-09-06 09:24:14 -0400

    Cambio menor a client.py

--- src/ev3/client.py
+++ src/ev3/client.py
@@ -3,7 +3,7 @@ import socket
 def retornar_socket():
     #"192.168.28.123"
     #"localhost"
-    HOST = "localhost"
+    HOST = "192.168.28.123"
     PORT = 8080
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
