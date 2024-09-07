ff08bbf93f176a32805763ce074c259f636bc9e1
Jeany-B <jeany.aravena.l@hotmail.com>
2024-09-07 10:58:32 -0400

    Cambios en las funciones para que se pueda mover el EV3 de forma correcta.

--- src/ev3/library.py
+++ src/ev3/library.py
@@ -8,29 +8,36 @@ from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
 from time import sleep
 
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, SpeedPercent, MoveTank, MediumMotor
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
 
 TANK_DRIVE = MoveTank(OUTPUT_A, OUTPUT_B)
+
+MOTOR_IZQUIERDO = LargeMotor(OUTPUT_A)
+MOTOR_DERECHO = LargeMotor(OUTPUT_B)
+MOTOR_APRETAR_GARRA = MediumMotor(OUTPUT_C)
+MOTOR_SUBIR_BAJAR = LargeMotor(OUTPUT_D)
+
 # GARRA = LargeMotor(OUTPUT_C)
 RAPIDO = SpeedPercent(100)
 MEDIO = SpeedPercent(50)
 LENTO = SpeedPercent(10)
 
 def avanzar():
-    TANK_DRIVE.on_for_seconds(MEDIO, MEDIO, 1)
+    TANK_DRIVE.on_for_seconds(-50, -50, 1)
 
 def retroceder():
-    TANK_DRIVE.on_for_seconds(SpeedPercent(-50), SpeedPercent(-50), 1)
+    TANK_DRIVE.on_for_seconds(50, 50, 1)
 
 def girar_derecha():
-    TANK_DRIVE.turn_right(MEDIO, 90)
+    TANK_DRIVE.on_for_seconds(-50, 50, 1)
 
 def girar_izquierda():
-    TANK_DRIVE.turn_right(MEDIO, -90)
+    TANK_DRIVE.on_for_seconds(50, -50, 1)
+    
 
 def hablar():
     sound = Sound()
