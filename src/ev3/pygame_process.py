39c3676e9f90a7aef14b4848c1b7585548e1c9b5
Jeany-B <jeany.aravena.l@hotmail.com>
2024-10-04 14:54:19 -0300

    Cambios en el archivo pygame_process.py para que pueda conectar un mando mientras se esté ejecutando (no funciona)

--- src/ev3/pygame_process.py
+++ src/ev3/pygame_process.py
@@ -3,61 +3,105 @@ import multiprocessing
 import time
 
 def pygame_process(queue):
+    
     pygame.init()
-
-    # Crear la ventana de Pygame
-    #screen = pygame.display.set_mode((640, 480))
-    #pygame.display.set_caption("Pygame Window")
-
-    #Joysticks
     pygame.joystick.init()
     joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 
+    def conectar_mando():
+        #pygame.joystick.quit()  # Reinicia el módulo de joystick para detectar nuevos dispositivos
+        pygame.joystick.init()
+        
+        #Comprobar si hay algun mando conectado
+        while not pygame.joystick.get_count() > 0:
+            #queue.put("Disconnected")
+            print("descon")
+            time.sleep(3)
+            #pygame.joystick.quit()
+            pygame.joystick.init()
+
+        print("conect")
+        #queue.put("Connected")
+    
+    conectar_mando()
+
+    # Define el tamaño de la ventana
+    ancho = 640
+    alto = 480
+
+    # Crea la ventana con las dimensiones especificadas
+    pantalla = pygame.display.set_mode((ancho, alto))
+
+    # Título de la ventana
+    pygame.display.set_caption("Ventana Simple")
+
+    # Color de fondo (RGB)
+    color_fondo = (0, 128, 255)  # Azul claro
+
+    
     running = True
     while running:
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 running = False
+            
+            if not pygame.joystick.get_count() > 0:
+                print("Mando desconecado")
+                #conectar_mando()
 
             #Se reconoce el movimiento con la funcion JOYHBUTTONDOWN (botones principales)
             if event.type == pygame.JOYBUTTONDOWN:
+                print("1if")
                 if pygame.joystick.Joystick(0).get_button(0):
-                    queue.put("EQUIS")
+                    print("x")
+                    #queue.put("EQUIS")
 
                 elif pygame.joystick.Joystick(0).get_button(1):
-                    queue.put("O")
+                    print("o")
+                    #queue.put("O")
 
                 elif pygame.joystick.Joystick(0).get_button(2):
-                    queue.put("CUADRADO")
+                    print("cua")
+                    #queue.put("CUADRADO")
 
                 elif pygame.joystick.Joystick(0).get_button(3):
-                    queue.put("TRIANGULO")
+                    print("tri")
+                    #queue.put("TRIANGULO")
 
             #Se reconoce el movimiento con la funcion JOYHATMOTION (flechitas)
             if event.type == pygame.JOYHATMOTION:
+                print("2if")
                 x, y = event.value
                 #Reconocer qué flecha es.
                 #Izquierda-Derecha
                 if (x == 1):
-                    queue.put("DERECHA")
+                    #queue.put("DERECHA")
+                    print("de")
                 elif (x == -1):
-                    queue.put("IZQUIERDA")
+                    #queue.put("IZQUIERDA")
+                    print("izq")
                 #Arriba-Abajo
                 elif (y == 1):
-                    queue.put("ARRIBA")
+                    #queue.put("ARRIBA")
+                    print("arrib")
                 elif (y == -1):
-                    queue.put("ABAJO")
+                    #queue.put("ABAJO")
+                    print("abaj")
                 else:
                     #print("Centro")
                     pass
                     
-        #screen.fill((0, 0, 0))
-        #pygame.display.flip()
+
+        # Rellena la pantalla con el color de fondo
+        pantalla.fill(color_fondo)
+        
+        # Actualiza la pantalla
+        pygame.display.flip()
 
         # Simular un retardo para la demostración
-        time.sleep(0.01)
-
+        #time.sleep(0.01)
+    
     pygame.quit()
 
 if __name__ == "__main__":
