2f57c50460d8993b440e4533bdf8d865329735fb
Jeany-B <jeany.aravena.l@hotmail.com>
2024-11-21 00:16:12 -0300

    Actualización/optimización del archivo, para ocupar menos recursos

--- src/ev3/pygame_process.py
+++ src/ev3/pygame_process.py
@@ -11,71 +11,104 @@ def pygame_process(queue):
         running = True
         while running:
             for event in pygame.event.get():
+                
+                # If si se conectó un mando
                 if event.type == pygame.JOYDEVICEADDED:
+                    queue.put("Mando conectado")
                     pygame.joystick.init()
-                    print("Mando conectado")
+                    #print("Mando conectado")
                     running = False
                     break
+                # Mensaje que todavía no hay mando conectado
+                #print("mando desconectado")
 
     joystick = pygame.joystick.Joystick(0)
+    posicion_anterior = (0, 0)
+
 
     running = True
     while running:
         event = pygame.event.wait()
-        print(event)
+        #print(event)
 
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.JOYDEVICEREMOVED:
+            #print("Mando desconectado")
+            queue.put("Mando desconectado")
             joystick.quit()
-            print("Mando desconectado")
 
         if event.type == pygame.JOYDEVICEADDED:
+            #print("Mando conectado")
+            queue.put("Mando conectado")
             joystick.init()
-            print("Mando conectado")
         
         #Se reconoce el movimiento con la funcion JOYHBUTTONDOWN (botones principales)
         if event.type == pygame.JOYBUTTONDOWN:
-            print("1if")
+            
             if pygame.joystick.Joystick(0).get_button(0):
                 print("x")
-                #queue.put("EQUIS")
+                queue.put("Si-Down")
 
             elif pygame.joystick.Joystick(0).get_button(1):
                 print("o")
-                #queue.put("O")
+                queue.put("Right")
 
             elif pygame.joystick.Joystick(0).get_button(2):
                 print("cua")
-                #queue.put("CUADRADO")
+                queue.put("Left")
 
             elif pygame.joystick.Joystick(0).get_button(3):
                 print("tri")
-                #queue.put("TRIANGULO")
+                queue.put("Up")
+        
+        if event.type == pygame.JOYBUTTONUP:
+            if event.button == 0:
+                print("No-Down")
+            if event.button == 1:
+                print("Se dejó de presionar o")
+            if event.button == 2:
+                print("Se dejó de presionar cua")
+            if event.button == 3:
+                print("Se dejó de presionar tri")
 
         #Se reconoce el movimiento con la funcion JOYHATMOTION (flechitas)
         if event.type == pygame.JOYHATMOTION:
-            print("2if")
+            posicion_actual = joystick.get_hat(0)
+
+            # Detectar cuando se presiona una dirección en el D-pad
+            if posicion_actual != (0, 0) and posicion_anterior == (0, 0):
+                print(f"Se presionó el D-pad en la dirección: {posicion_actual}")
+
+            # Detectar cuando se suelta una dirección en el D-pad
+            elif posicion_actual == (0, 0) and posicion_anterior != (0, 0):
+                print(f"Se soltó el D-pad de la dirección: {posicion_anterior}")
+
+            # Actualizar la posición anterior del D-pad
+            posicion_anterior = posicion_actual
+
+
             x, y = event.value
             #Reconocer qué flecha es.
             #Izquierda-Derecha
             if (x == 1):
-                #queue.put("DERECHA")
-                print("de")
+                queue.put("Si-d")
+                print("d")
             elif (x == -1):
-                #queue.put("IZQUIERDA")
-                print("izq")
+                queue.put("a")
+                print("a")
             #Arriba-Abajo
             elif (y == 1):
-                #queue.put("ARRIBA")
-                print("arrib")
+                queue.put("w")
+                print("w")
             elif (y == -1):
-                #queue.put("ABAJO")
-                print("abaj")
+                queue.put("s")
+                print("s")
             else:
-                #print("Centro")
+                #queue.put()
                 pass
+
                 
 
         # Simular un retardo para la demostración
