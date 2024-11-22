6bbeda3265711a1ecdb8d844aeb47fd66e034947
Jeany-B <jeany.aravena.l@hotmail.com>
2024-11-21 23:58:03 -0300

    Feature: Se modifican las funciones de movimiento, esto es para cuando presiona una tecla/boton se mueva y cuando suelte ese boton/tecla se deje de mover

--- src/ev3/library.py
+++ src/ev3/library.py
@@ -10,11 +10,12 @@ from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
 
 TANK_DRIVE = MoveTank(OUTPUT_A, OUTPUT_B)
+#TANK_CLAW = MoveTank(OUTPUT_C, OUTPUT_D)
 JOYSTICK_DRIVE = MoveJoystick(OUTPUT_A, OUTPUT_B)
 
 MOTOR_IZQUIERDO = LargeMotor(OUTPUT_A)
 MOTOR_DERECHO = LargeMotor(OUTPUT_B)
-MOTOR_SUBIR_BAJAR = LargeMotor(OUTPUT_C)
+MOTOR_SUBIR_BAJAR = MediumMotor(OUTPUT_C)
 MOTOR_APRETAR_GARRA = MediumMotor(OUTPUT_D)
 
 RAPIDO = SpeedPercent(100)
@@ -36,16 +37,25 @@ def avanzar():
     TANK_DRIVE.on(-50, -50)
 
 def retroceder():
-    TANK_DRIVE.on_for_seconds(50, 50, 1)
+    TANK_DRIVE.on(50, 50)
+    #TANK_DRIVE.on_for_seconds(50, 50, 1)
 
 def girar_derecha():
-    TANK_DRIVE.on_for_seconds(-50, 50, 1)
+    TANK_DRIVE.on(50, -50)
+    #TANK_DRIVE.on_for_seconds(-50, 50, 1)
 
 def girar_izquierda():
-    TANK_DRIVE.on_for_seconds(50, -50, 1)
+    TANK_DRIVE.on(-50, 50)
+    #TANK_DRIVE.on_for_seconds(50, -50, 1)
+
+def parar_garra():
+    MOTOR_SUBIR_BAJAR.on(0,0)
+    MOTOR_APRETAR_GARRA.on(0,0)
+
 
 def subir_garra():
-    MOTOR_SUBIR_BAJAR.on_for_seconds(-40, 1)
+    print(MOTOR_SUBIR_BAJAR.position())
+    #MOTOR_SUBIR_BAJAR.on_for_seconds(-40, 1)
 
 def bajar_garra():
     MOTOR_SUBIR_BAJAR.on_for_seconds(40, 1)
